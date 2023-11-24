8369436f1d37e1bb90ef8b06e8c3b5d23e3b47d0
DavidMunoz18 <145343117+DavidMunoz18@users.noreply.github.com>
2023-11-24 14:43:51 +0100

    Add files via upload

---
+++ edu.ejercicio3/edu.ejercicio3/src/Controladores/inicio.java
@@ -0,0 +1,12 @@
+package Controladores;
+
+public class inicio {
+
+	public static void main(String[] args) {
+		
+		
+
+		
+	}
+
+}
---
+++ edu.ejercicio3/edu.ejercicio3/src/Servicios/OperativaInterfaz.java
@@ -0,0 +1,8 @@
+package Servicios;
+
+public interface OperativaInterfaz {
+
+	public int numeroAleatorio();
+	
+	public int compararNumero(int numeroRandom);
+}
